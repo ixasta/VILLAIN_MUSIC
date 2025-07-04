e5abcd0987fdd78d4625c1c89075061dd5028d9e
ixasta <saifizishan33@gmail.com>
2025-07-04 16:19:52 +0530

    Update qr.py

--- VILLAIN_MUSIC/plugins/yumi/qr.py
+++ VILLAIN_MUSIC/plugins/yumi/qr.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import qrcode
-from VILLAIN_MUSIC import app
+from ASTA_MUSIC import app
 from PIL import Image
 import io
 
