12c35c92193642f6a3a6aac9c64862457d733b95
ixasta <saifizishan33@gmail.com>
2025-07-04 17:36:54 +0530

    Update resume.py

--- VILLAIN_MUSIC/plugins/admins/resume.py
+++ VILLAIN_MUSIC/plugins/admins/resume.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from VILLAIN_MUSIC import app
-from VILLAIN_MUSIC.core.call import VILLAIN
-from VILLAIN_MUSIC.utils.database import is_music_playing, music_on
-from VILLAIN_MUSIC.utils.decorators import AdminRightsCheck
-from VILLAIN_MUSIC.utils.inline import close_markup
+from ASTA_MUSIC import app
+from ASTA_MUSIC.core.call import ASTA
+from ASTA_MUSIC.utils.database import is_music_playing, music_on
+from ASTA_MUSIC.utils.decorators import AdminRightsCheck
+from ASTA_MUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 
