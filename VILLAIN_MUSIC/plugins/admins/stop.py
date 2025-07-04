64e3c0084c5d8f202d1c7d2fb6bd7cc61dd19d7e
ixasta <saifizishan33@gmail.com>
2025-07-04 17:26:17 +0530

    Update stop.py

--- VILLAIN_MUSIC/plugins/admins/stop.py
+++ VILLAIN_MUSIC/plugins/admins/stop.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from VILLAIN_MUSIC import app
-from VILLAIN_MUSIC.core.call import VILLAIN
-from VILLAIN_MUSIC.utils.database import set_loop
-from VILLAIN_MUSIC.utils.decorators import AdminRightsCheck
-from VILLAIN_MUSIC.utils.inline import close_markup
+from ASTA_MUSIC import app
+from ASTA_MUSIC.core.call import ASTA
+from ASTA_MUSIC.utils.database import set_loop
+from ASTA_MUSIC.utils.decorators import AdminRightsCheck
+from ASTA_MUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -18,7 +18,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await VILLAIN.stop_stream(chat_id)
+    await ASTA.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention),
