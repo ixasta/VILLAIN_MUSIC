9bb1a56c4f7fb11d06f6ed3ce7debaaa4ee325fa
ixasta <saifizishan33@gmail.com>
2025-07-04 13:32:53 +0530

    Update bot.py

--- VILLAIN_MUSIC/core/bot.py
+++ VILLAIN_MUSIC/core/bot.py
@@ -10,7 +10,7 @@ class VILLAIN(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="VILLAIN_MUSIC",
+            name="ASTA_MUSIC",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
