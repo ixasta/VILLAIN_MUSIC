96d9a12699a4e209d67de2b72a4519e859b49dc6
ixasta <saifizishan33@gmail.com>
2025-07-04 17:34:07 +0530

    Update pause.py

--- VILLAIN_MUSIC/plugins/admins/pause.py
+++ VILLAIN_MUSIC/plugins/admins/pause.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from VILLAIN_MUSIC import app
-from VILLAIN_MUSIC.core.call import VILLAIN
-from VILLAIN_MUSIC.utils.database import is_music_playing, music_off
-from VILLAIN_MUSIC.utils.decorators import AdminRightsCheck
-from VILLAIN_MUSIC.utils.inline import close_markup
+from ASTA_MUSIC import app
+from ASTA_MUSIC.core.call import ASTA
+from ASTA_MUSIC.utils.database import is_music_playing, music_off
+from ASTA_MUSIC.utils.decorators import AdminRightsCheck
+from ASTA_MUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -15,7 +15,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await VILLAIN.pause_stream(chat_id)
+    await ASTA.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
