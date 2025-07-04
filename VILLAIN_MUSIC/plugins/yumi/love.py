7e3267c40adc2bea3117b2b4e5f97b55e2c8065b
ixasta <saifizishan33@gmail.com>
2025-07-04 16:27:10 +0530

    Update love.py

--- VILLAIN_MUSIC/plugins/yumi/love.py
+++ VILLAIN_MUSIC/plugins/yumi/love.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import random
-from VILLAIN_MUSIC import app
+from ASTA_MUSIC import app
 
 def get_random_message(love_percentage):
     if love_percentage <= 30:
